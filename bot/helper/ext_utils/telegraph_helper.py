5d725a647b636fa2dbcc7f2ddc691556114904dd
Akaisera17 <106355412+Akaisera17@users.noreply.github.com>
2022-07-13 08:42:02 +0700

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -78,4 +78,4 @@ class TelegraphHelper:
         return
 
 
-telegraph=TelegraphHelper('Z-Mirror-Bot', 'https://github.com/shuvam-dawn/MLTB-Custom-UI')
+telegraph=TelegraphHelper('Window-Mirror-Bot', 'https://github.com/Akaisera17/Mltb-New')

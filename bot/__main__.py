1e2ce767c3945c3330cbaa075f8043143d443c16
Akaisera17 <106355412+Akaisera17@users.noreply.github.com>
2022-07-13 08:04:08 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -48,11 +48,11 @@ def start(update, context):
     buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
     buttons.buildbutton("Repo", "https://github.com/Dawn-India/Z-Mirror")
     buttons.buildbutton("Mirror Group", "https://t.me/z_mirror")
-    buttons.buildbutton("Owner", "https://t.me/dawn_in")
+    buttons.buildbutton("Owner", "https://t.me/faishalk")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-Welcome | Z Mirror service is ready for you
+Welcome | Window Mirror service is ready for you
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
@@ -153,7 +153,7 @@ help_string_telegraph = f'''<br>
 '''
 
 help = telegraph.create_page(
-        title='Z-Mirror-Bot Help',
+        title='Window-Mirror-Bot Help',
         content=help_string_telegraph,
     )["path"]
 

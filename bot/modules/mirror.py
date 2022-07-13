0f5e1f036af4071d1c326a4600f799bd78ee1459
Akaisera17 <106355412+Akaisera17@users.noreply.github.com>
2022-07-13 08:14:11 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -210,9 +210,9 @@ class MirrorListener:
             msg += f'\n<b>Total Files: </b>{folders}'
             if typ != 0:
                 msg += f'\n<b>Corrupted Files: </b>{typ}'
-            msg += f'\n\n<b>Hey </b>{self.tag} <b>Your Job is Done</b>'
-            msg += f'\n<b>It Tooks:</b> {get_readable_time(time() - self.message.date.timestamp())}'
-            msg += f'\n\n<b>Thanks For using @Z_Mirror</b>'
+            msg += f'\n\n<b>🥷 Hey </b>{self.tag} <b>Your Job is Done</b>'
+            msg += f'\n<b>⏱️ Elapsed Time :</b> {get_readable_time(time() - self.message.date.timestamp())}'
+            msg += f'\n\n<b>Silahkan Download Melalui Link di Bawah Ini 👇</b>'
             if not files:
                 sendMessage(msg, self.bot, self.message)
             else:
@@ -230,9 +230,9 @@ class MirrorListener:
             if ospath.isdir(f'{DOWNLOAD_DIR}{self.uid}/{name}'):
                 msg += f'\n<b>SubFolders: </b>{folders}'
                 msg += f'\n<b>Files: </b>{files}'
-            msg += f'\n\n<b>Hey </b>{self.tag} <b>Your Job is Done</b>'
-            msg += f'\n<b>It Tooks:</b> {get_readable_time(time() - self.message.date.timestamp())}'
-            msg += f'\n\n<b>Thanks For using @Z_Mirror</b>'
+            msg += f'\n\n<b>🥷 Hey </b>{self.tag} <b>Your Job is Done</b>'
+            msg += f'\n<b>⏱️ Elapsed Time :</b> {get_readable_time(time() - self.message.date.timestamp())}'
+            msg += f'\n\n<b>Silahkan Download Melalui Link di Bawah Ini 👇</b>'
             buttons = ButtonMaker()
             link = short_url(link)
             buttons.buildbutton("☁️ Drive Link", link)
